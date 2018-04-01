30986989176c16d2b5b377eec1307c3bb8e1ab87
Ronald <ronald@playa.hk>
2018-04-02 01:46:19 +0800

    [Angular Firebase 入門與實做] Day-01 開發環境建立與初始化

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import {environment} from '../environments/environment';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase), // init
+    AngularFireDatabaseModule, // add realtime DB module
+    AngularFirestoreModule, // add cloudstore DB module
+    AngularFireAuthModule, // add auth module
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  // firebase: {
+  //   apiKey: "AIzaSyACyYiDwVOVeHO08Pl_5FrfiPA08VAPdcc",
+  //   authDomain: "ronald-s-firebase-project.firebaseapp.com",
+  //   databaseURL: "https://ronald-s-firebase-project.firebaseio.com",
+  //   projectId: "ronald-s-firebase-project",
+  //   storageBucket: "ronald-s-firebase-project.appspot.com",
+  //   messagingSenderId: "574653385438"
+  // }
+};
